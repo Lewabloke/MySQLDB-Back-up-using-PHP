f8b7e20ec519fe912edb7884586cfee9a16e1024
Omolewa Stephen Ayobami <Omolewastephen@gmail.com>
2017-11-20 12:04:26 +0100

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,9 @@
+<?php
 $db_hostname = "localhost";
 $db_database = "dbName";
 $db_username = "root";
 $db_password = "";
 
 $DBH = new PDO("mysql:host=$db_hostname;dbname=$db_database; charset=utf8", $db_username, $db_password);
+
+?>
