939c053e6b5d62b689cf88a9b180dd1b4b8f5408
Omolewa Stephen Ayobami <Omolewastephen@gmail.com>
2017-11-20 12:05:21 +0100

    Update database.php
    Database connection file

--- database.php
+++ database.php
@@ -1,9 +1,12 @@
 <?php
+
+// Connection Paramters
 $db_hostname = "localhost";
 $db_database = "dbName";
 $db_username = "root";
 $db_password = "";
 
+// Coneection Handle
 $DBH = new PDO("mysql:host=$db_hostname;dbname=$db_database; charset=utf8", $db_username, $db_password);
 
 ?>
